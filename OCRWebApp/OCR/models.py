4f88a11f5152bf5d773e4b4236b2246517aa2fda
Sushree Nadiminty <90982755+sushree-20@users.noreply.github.com>
2021-10-05 00:34:04 +0530

    Update models.py

--- OCRWebApp/OCR/models.py
+++ OCRWebApp/OCR/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Ocr(models.Model):
     image = models.ImageField(upload_to='images/')
+    
+class AudioFile(models.Model):
+    obj = models.FileField(upload_to="static/audio/")
